811038b6c01a0221d0f7f2e653b14a5d53ae0654
Fernanda Giannini <elcambionv@gmail.com>
2023-07-29 18:43:01 -0300

    make ejercicio 1

---
+++ problem1-init.js
@@ -0,0 +1,12 @@
+
+const multipleOfThree = (number) => (number % 3 === 0)
+let numberToChek = 302
+let isMultipleOfThree = multipleOfThree (numberToChek)
+
+console.log(isMultipleOfThree)
+
+/*la funcion multipleOfThree recibe un numero y chequea que sea multiplo de 3. Si es así 
+devuelve true, sino devuelve false.
+A la variable numberToCheck se asigna el valor a chequear y es el que se pasa por parámetro 
+a la función multipleOfThree*/
+
